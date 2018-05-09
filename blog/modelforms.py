ae8a7f751466cb6f3e846bc76ffb66f3c9dd2721
crowlee <machunnamoon@hanmail.net>
2018-05-09 13:30:53 +0900

    django secDay first commit
    fomr create

--- blog/modelforms.py
+++ blog/modelforms.py
@@ -1,8 +1,17 @@
 from django import forms
-from .models import  import Post
+from .models import Post
+
+
+def min_length_3_validator(value):
+    if len(value) < 3:
+        raise forms.ValidationError('3글자 이상 입력해 주세요')
 
 class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text']
-        
+
+
+class PostForm(forms.Form):
+    title = forms.CharField(validators=[min_length_3_validator])
+    text = forms.CharField(widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.utils import timezone
+
+from blog.modelforms import PostModelForm, PostForm
 from .models import  Post
+
 # Create your views here.
 
 #글 목록보기
@@ -18,4 +21,39 @@ def post_list(request):
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html',{'post':post})
+
+#글 등록
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm()
+        print(form)
+
+
+    return render(request, 'blog/post_edit.html',{'form':form})
+
+#글등록 폼을 사용
+def post_new_form(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = Post(author = request.user, title = form.title, text = form.text, published_data = timezone.now())
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        else:
+            print(form.errors)
+    else:
+        form = PostForm()
+    return  render(request, 'blog/post_form.html',{'form':form})
+
 
